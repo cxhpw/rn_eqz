3d3c30535088fa68bd9e0d1aeab2d63220232e41
AaronCxh <xiaohongcwork@gmail.com>
2023-03-08 15:47:44 +0800

    refactor: 修改bootsplash和appIcon

---
+++ src/components/Swipeable/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const Index = () => {
+  return (
+    <View>
+      <Text>Index</Text>
+    </View>
+  );
+};
+Index.displayName = 'Swiperable';
+
+export default Index;

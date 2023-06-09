e86e6969f56a6e98c376b12efa221fc1f9518f05
AaronCxh <xiaohongcwork@gmail.com>
2023-06-09 15:39:09 +0800

    refactor: 配置修改

--- src/App.tsx
+++ src/App.tsx
@@ -19,8 +19,8 @@ import {
 } from '@react-navigation/native';
 import { useFlipper } from '@react-navigation/devtools';
 import { enableFreeze } from 'react-native-screens';
-import { Fallback, ThemeProvider, ErrorBlock } from '@/components';
-import { useSafeState, useMemoizedFn, useMount } from 'ahooks';
+import { Fallback, ThemeProvider } from '@/components';
+import { useSafeState, useMemoizedFn } from 'ahooks';
 import { useNetwork } from './hooks';
 import { navigationRef } from '@/services/NavigationService';
 import { linking } from './linking';
